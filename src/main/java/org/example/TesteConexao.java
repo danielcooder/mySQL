bb7dc2e427b98893a02395ac8a31b6bb24a6b986
daniel cooder <danielcooder@gmail.com>
2025-02-03 17:51:39 -0300

    implementcao de CRUD para interacao com banco de dados MySQL

---
+++ src/main/java/org/example/TesteConexao.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) throws SQLException {
+
+       final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+       final String usuario = "root";
+       final String senha = "1995@Daniel";
+
+        Connection conexao = DriverManager.getConnection(url, usuario, senha);
+
+        System.out.println();
+
+        System.out.println("Conexão efetuada com sucesso!");
+
+    }
+}
